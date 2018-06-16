14c7b12feb07c30695c20404930d6ad5538b0d36
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-16 15:54:30 +0200

    removed console log, index

--- src/reducers/swCharacters.js
+++ src/reducers/swCharacters.js
@@ -1,15 +1,11 @@
 const initialState = {
     page: 1,
     isLoading: true,
-    // todo check how can I send index in other way
-    index: 0,
     selected: {},
     results: {}
 };
 
 export default function swCharacters(state = initialState, action) {
-    // console.log('state', state);
-    // console.log('action', action);
     switch (action.type) {
         case 'FETCH_USERS':
             return {
@@ -28,8 +24,7 @@ export default function swCharacters(state = initialState, action) {
         case 'SELECTED_CHARACTER':
             return {
                 ...state,
-                selected: action.selected,
-                index: action.index
+                selected: action.selected
             };
 
         default:
