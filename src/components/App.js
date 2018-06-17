9f7d09cfaf82e762674ae073a38a8171dc886c46
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-17 22:48:11 +0200

    remove navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
-    Route,
-    Link
+    Route
 } from 'react-router-dom';
 
 import FetchUsers from './FetchUsers';
@@ -10,19 +9,13 @@ import FetchUsers from './FetchUsers';
 import Home from './Home';
 import About from './About';
 
-// todo finish navigation (make it pretty and shiny)
-
 class App extends Component {
     render() {
         return (
             <Router>
                 <div className="container">
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                    </ul>
                     <FetchUsers/>
+
                     <Route exact path="/" component={Home}/>
                     <Route path="/people/" component={About}/>
                 </div>
