ce6e0985923819a919c6fd85d85f892c92758e25
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-17 22:46:41 +0200

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Logo from './Logo';
+
+const Header = () => {
+    return (
+        <header>
+            <Logo/>
+        </header>
+    );
+};
+
+export default Header;
