923995767dd2f6a9d1dc915ba078d9e3325e2160
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-17 22:59:31 +0200

    cleaner code

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,9 +3,10 @@ const numStars = 200;
 
 // For every star we want to display
 for (let i = 0; i < numStars; i++) {
-    let star = document.createElement("div");
+    let star = document.createElement("div"),
+        xy = getRandomPosition();
+
     star.className = "star";
-    let xy = getRandomPosition();
     star.style.top = xy[0] + 'px';
     star.style.left = xy[1] + 'px';
     document.body.append(star);
@@ -13,9 +14,10 @@ for (let i = 0; i < numStars; i++) {
 
 // Gets random x, y values based on the size of the container
 function getRandomPosition() {
-    let y = window.innerWidth;
-    let x = window.innerHeight;
-    let randomX = Math.floor(Math.random()*x);
-    let randomY = Math.floor(Math.random()*y);
+    let innerWidth = window.innerWidth,
+        innerHeight = window.innerHeight,
+        randomX = Math.floor(Math.random()*innerWidth),
+        randomY = Math.floor(Math.random()*innerHeight);
+
     return [randomX,randomY];
 }
