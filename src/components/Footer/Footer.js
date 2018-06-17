cf9dd4f97eefd33a5250a59723ebe5f3c0b0a9d8
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-18 01:22:02 +0200

    missing Footer.js file

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h4>How to use this App?</h4>
+            <p>Choose your character and click on it to see details.</p>
+            <p>Use pagination to see another ten characters.</p>
+            <p>You can use 'Back' button or click on Star Wars logo to go back to Homepage.</p>
+        </footer>
+    );
+};
+
+export default Footer;
