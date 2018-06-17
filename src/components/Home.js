4de446e1143c5fb54aadb6f2daf5eeb09d880ca3
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-17 22:53:41 +0200

    header added to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from './Header/Header';
 import ResultsList from './ResultsList';
 import Pagination from './Pagination';
 
 const Home = () => {
     return (
         <div>
+            <Header/>
             <ResultsList/>
             <Pagination/>
         </div>
