51a98fe2185da846fae0b0c5f3b164c96b26a8dc
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-18 16:53:19 +0200

    back button added todo

--- src/components/Utils/backHomeButton.js
+++ src/components/Utils/backHomeButton.js
@@ -7,6 +7,7 @@ import {Button} from 'react-bootstrap';
 const backHomeButton = ( props ) => {
     let { text } = props;
 
+    // todo back to previous page (same as click on SW logo)
     return (
         <Link to='/'>
             <Button>{text ? text : 'Back to home'}</Button>
