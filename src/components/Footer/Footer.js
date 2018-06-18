0040069f75ebe5fe011a9fe6e05240d784084b1c
Tomasz Pach <tomasz@nurtureagency.com>
2018-06-18 13:25:25 +0200

    footer question mark and popover

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {
+    Glyphicon,
+    ButtonToolbar,
+    OverlayTrigger,
+    Popover
+} from 'react-bootstrap';
 
 const Footer = () => {
-    return (
-        <footer>
-            <h4>How to use this App?</h4>
+    const popoverLeft = (
+        <Popover bsClass="how-to popover" id="popover-positioned-left" title="How to use this?">
             <p>Choose your character and click on it to see details.</p>
             <p>Use pagination to see another ten characters.</p>
             <p>You can use 'Back' button or click on Star Wars logo to go back to Homepage.</p>
+        </Popover>
+    );
+
+    return (
+        <footer>
+            <ButtonToolbar>
+                <OverlayTrigger trigger="click" placement="left" overlay={popoverLeft}>
+                    <Glyphicon glyph="question-sign"/>
+                </OverlayTrigger>
+            </ButtonToolbar>
         </footer>
     );
 };
